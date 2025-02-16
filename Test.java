b241c8995effcc5cdca84d9e1fc02348e4d24899
bcrow99 <59273209+bcrow99@users.noreply.github.com>
2025-02-16 05:57:50 -0800

    partly working
    Works when the x dimension is equal to the y dimension and not equal to a power of two

--- Test.java
+++ Test.java
@@ -14,25 +14,20 @@ import javax.swing.event.ChangeListener;
 public class Test
 {
 	BufferedImage original_image;
-	BufferedImage image;
+	BufferedImage extracted_image;
 	ImageCanvas   image_canvas;
 	
 	int           x, y;
 	int           image_x_offset, image_y_offset;
 	int           canvas_x_offset, canvas_y_offset;
 	int           image_xdim, image_ydim;
+	int           extracted_xdim, extracted_ydim;
 	int           scaled_xdim, scaled_ydim;
 	int           canvas_xdim, canvas_ydim;
 	
 	int           width, height;
 	
 	int []        pixel;
-
-	double        scale, xposition, yposition;
-	
-	JScrollBar    xscrollbar, yscrollbar;
-	
-	boolean       mouse_changing_scrollbar = false;
 	
 	ArrayList     channel_list;
 	
@@ -61,14 +56,20 @@ public class Test
 			image_ydim = original_image.getHeight();
 			
 			System.out.println("Xdim is " + image_xdim);
-			
-			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-			width  = (int)screenSize.getWidth();
-			height = (int)screenSize.getHeight();
-		
 			System.out.println("Xdim = " + image_xdim + ", ydim = " + image_ydim);
-			// This is less resolution than the system settings.
-			System.out.println("Screen width is " + width + ", screen height is " + height);
+			
+			
+			int power_of_two_xdim = 2;
+			while(power_of_two_xdim < image_xdim)
+				power_of_two_xdim *= 2;
+			
+			System.out.println("Nearest power of two xdim is " + power_of_two_xdim);
+			
+			int power_of_two_ydim = 2;
+			while(power_of_two_ydim < image_ydim)
+				power_of_two_ydim *= 2;
+			
+			System.out.println("Nearest power of two ydim is " + power_of_two_ydim);
 			System.out.println();
 		  
 			JFrame frame = new JFrame("Displaying " + filename);
@@ -81,50 +82,18 @@ public class Test
 			    }
 			};
 			frame.addWindowListener(window_handler);
-			    
+			 
 			image_canvas = new ImageCanvas();
-				
-				
-			if(image_xdim <= (width - 20) && image_ydim <= (height - 60))
-			{
-				canvas_xdim = image_xdim;
-				canvas_ydim = image_ydim;
-				scale       = 1.;
-			}
-			else if(image_xdim <= (width - 20))
-			{
-				canvas_ydim = height - 60; 
-				scale       = canvas_ydim;
-				scale      /= image_ydim;
-				canvas_xdim = (int)(scale * image_xdim);
-			}
-			else if(image_ydim <= (height - 60))
-            {
-				canvas_xdim = width - 20; 
-				scale       = canvas_ydim;
-				scale      /= image_ydim;
-            }
-			else
-			{
-				double xscale = width - 20;
-				xscale       /= image_xdim;
-				double yscale = height - 60;
-				yscale       /= image_ydim;
-				    
-				if(xscale <= yscale)
-				    scale = xscale;
-				else
-				    scale = yscale;
-				    
-				canvas_xdim = (int)(scale * image_xdim);
-				canvas_ydim = (int)(scale * image_ydim);
-			}
 			
+			extracted_xdim = 640;
+			extracted_ydim = 640;
+			image_canvas.setSize(extracted_xdim, extracted_ydim);
 			
-			image_canvas.setSize(canvas_xdim, canvas_ydim);
+			pixel = new int[extracted_xdim * extracted_ydim];
 			
-			pixel = new int[image_xdim * image_ydim];
-			PixelGrabber pixel_grabber = new PixelGrabber(original_image, 0, 0, image_xdim, image_ydim, pixel, 0, image_xdim);
+			extracted_image = original_image.getSubimage(0,  0,  extracted_xdim,  extracted_ydim);
+			
+			PixelGrabber pixel_grabber = new PixelGrabber(extracted_image, 0, 0, extracted_xdim, extracted_ydim, pixel, 0, extracted_xdim);
 	        try
 	        {
 	            pixel_grabber.grabPixels();
@@ -138,12 +107,12 @@ public class Test
 	            System.err.println("Error grabbing pixels.");
 	            System.exit(1);
 	        }
-	        
-	        int [] alpha = new int[image_xdim * image_ydim];
-	        int [] blue  = new int[image_xdim * image_ydim];
-	        int [] green = new int[image_xdim * image_ydim];
-	        int [] red   = new int[image_xdim * image_ydim];
-	        for(int i = 0; i < image_xdim * image_ydim; i++)
+	       
+	        int [] alpha = new int[extracted_xdim * extracted_ydim];
+	        int [] blue  = new int[extracted_xdim * extracted_ydim];
+	        int [] green = new int[extracted_xdim * extracted_ydim];
+	        int [] red   = new int[extracted_xdim * extracted_ydim];
+	        for(int i = 0; i < extracted_xdim * extracted_ydim; i++)
 			{
 		        alpha[i] = (pixel[i] >> 24) & 0xff;
 			    blue[i]  = (pixel[i] >> 16) & 0xff;
@@ -155,10 +124,11 @@ public class Test
 	        channel_list.add(green);
 	        channel_list.add(red);
 	        
-	        int [] blue_wavelets = WaveletMapper.forward_transform(blue, image_xdim);
-	        int [] blue2         = WaveletMapper.inverse_transform(blue_wavelets, image_xdim);
-	        int [] pixel         = WaveletMapper.getPixel(blue2, blue2, blue2, image_xdim);
-	        original_image.setRGB(0, 0, image_xdim, image_ydim, pixel, 0, image_xdim);
+	        //int [] resized_blue  = ResizeMapper.resize(blue, image_xdim, 1024, 1024);
+	        int [] blue_wavelets = WaveletMapper.forward_transform(blue, extracted_xdim);
+	        int [] blue2         = WaveletMapper.inverse_transform(blue_wavelets, extracted_xdim);
+	        int [] pixel         = WaveletMapper.getPixel(blue, blue, blue, extracted_xdim);
+	        extracted_image.setRGB(0, 0, extracted_xdim, extracted_ydim, pixel, 0, extracted_xdim);
 	        
 	        
 	        /*
@@ -175,218 +145,17 @@ public class Test
 	        original_image.setRGB(0, 0, image_xdim, image_ydim, pixel, 0, image_xdim);
 	        */
 			
-			JScrollBar xscrollbar = new JScrollBar(JScrollBar.HORIZONTAL, 49, 1, 0, 98);
-			AdjustmentListener xhandler = new AdjustmentListener()
-			{
-			    public void adjustmentValueChanged(AdjustmentEvent event)
-				{
-			    	if(!mouse_changing_scrollbar)
-			    	{
-			    	    boolean still_changing = event.getValueIsAdjusting();
-			    	    if(still_changing == false)
-			    	    {
-				            int position = event.getValue();
-				            //System.out.println("Position of xscrollbar is now " + position);
-				            yposition  = position;
-				            yposition /= 98;
-			    	    }
-			    	    image_canvas.repaint();
-			    	}
-				}
-			};		
-			xscrollbar.addAdjustmentListener(xhandler);
-			
-			JScrollBar yscrollbar = new JScrollBar(JScrollBar.VERTICAL, 49, 1, 0, 98);
-			AdjustmentListener yhandler = new AdjustmentListener()
-			{
-			    public void adjustmentValueChanged(AdjustmentEvent event)
-				{
-			    	if(!mouse_changing_scrollbar)
-			    	{
-			    	    boolean still_changing = event.getValueIsAdjusting();
-			    	    if(still_changing == false)
-			    	    {
-				            int position = event.getValue();
-				            //System.out.println("Position of yscrollbar is now " + position);
-				            yposition  = position;
-				            yposition /= 98;
-			    	    }
-			    	    image_canvas.repaint();
-			    	}
-				}
-			};		
-			yscrollbar.addAdjustmentListener(yhandler);
-			
-			
-			if(scaled_xdim <= canvas_xdim)
-				xscrollbar.setEnabled(false);
-			if(scaled_ydim <= canvas_ydim)
-				yscrollbar.setEnabled(false);
-		
-			MouseAdapter zoom_handler = new MouseAdapter()
-			{
-				public void mouseClicked(MouseEvent event)
-				{
-					int x          = event.getX();
-					int y          = event.getY();
-					
-					
-					
-					
-					
-					int range     = scaled_xdim - canvas_xdim;
-					image_x_offset  = (int)(range * xposition);
-					
-					image_x_offset += x;
-					image_x_offset -= canvas_xdim / 2;
-					if(image_x_offset > range)
-						image_x_offset = (int)range;
-					else if(image_x_offset < 0)
-						image_x_offset = 0;
-					xposition       = image_x_offset;
-					xposition      /= range;
-					int x_value     = (int)(xposition * 98.);
-					
-					
-					range           = scaled_ydim - canvas_ydim;
-					image_y_offset  = (int)(range * yposition);
-					image_y_offset += y;
-					image_y_offset -= canvas_ydim / 2;
-					if(image_y_offset > range)
-						image_y_offset = (int)range;
-					else if(image_y_offset < 0)
-						image_y_offset = 0;
-					yposition       = image_y_offset;
-					yposition      /= range;
-					int y_value     = (int)(yposition * 98.);
-					
-					
-					mouse_changing_scrollbar = true;
-					xscrollbar.setValue(x_value);
-					yscrollbar.setValue(y_value);
-					mouse_changing_scrollbar = false;
-					
-					int button = event.getButton();
-					if(button == 1)
-					{
-						System.out.println("Zooming in at x = " + x + ", y = " + y);
-						scale *= 1.1;
-						
-						
-						if(scale * image_xdim > canvas_xdim && !xscrollbar.isEnabled())
-						{
-						    xscrollbar.setEnabled(true);   	
-						}
-						if(scale * image_ydim > canvas_ydim && !yscrollbar.isEnabled())
-						{
-							yscrollbar.setEnabled(true); 	
-						}
-					}
-					else if (button == 3)
-					{
-						System.out.println("Zooming out at x = " + x + ", y = " + y);
-						scale *= 0.9;
-						
-						if(scale * image_xdim < canvas_xdim && xscrollbar.isEnabled())
-						{
-						    xscrollbar.setEnabled(false);   	
-						}
-						if(scale * image_ydim < canvas_ydim && yscrollbar.isEnabled())
-						{
-							yscrollbar.setEnabled(false); 	
-						}
-					}
-					image_canvas.repaint();
-				}	
-			};
-			
-			image_canvas.addMouseListener(zoom_handler);
-			
-			
-			MouseAdapter move_handler = new MouseAdapter()
-			{
-				int start_x, end_x;
-				int start_y, end_y;
-			
-				public void mousePressed(MouseEvent event)
-				{
-					start_x  = event.getX();
-					start_y  = event.getY();	
-				}
-				
-				public void mouseReleased(MouseEvent event)
-				{
-					end_x  = event.getX();
-					end_y  = event.getY();	
-					
-					if(end_x != start_x || end_y != start_y)
-					{
-						System.out.println("Moved from x = " + start_x + ", y = " + start_y + " to x = " + end_x + ", y = " + end_y);
-						
-						
-						int delta_x     = end_x - start_x;
-						scaled_xdim     = (int)(scale * image_xdim);
-						double range    = scaled_xdim - canvas_xdim;
-						image_x_offset  = (int)(range * xposition);
-						image_x_offset += delta_x;
-						if(image_x_offset > range)
-							image_x_offset = (int)range;
-						else if(image_x_offset < 0)
-							image_x_offset = 0;
-						
-						xposition       = image_x_offset;
-						xposition      /= range;
-						int x_value     = (int)(xposition * 98.);
-						
-						int delta_y     = end_y - start_y;
-						scaled_ydim     = (int)(scale * image_ydim);
-						range           = scaled_ydim - canvas_ydim;
-						image_y_offset  = (int)(range * yposition);
-						image_y_offset += delta_y;
-						if(image_y_offset > range)
-							image_y_offset = (int)range;
-						else if(image_y_offset < 0)
-							image_y_offset = 0;
-						
-						yposition       = image_y_offset;
-						yposition      /= range;
-						int y_value     = (int)(yposition * 98.);
-						mouse_changing_scrollbar = true;
-						xscrollbar.setValue(x_value);
-						yscrollbar.setValue(y_value);
-						mouse_changing_scrollbar = false;
-						image_canvas.repaint();
-					}
-				}
-			};
-			
-			image_canvas.addMouseListener(move_handler);
-			
-			x               = canvas_xdim / 2;
-			y               = canvas_ydim / 2;
-			image_x_offset  = 0;
-			image_y_offset  = 0;
-			canvas_x_offset = 0;
-			canvas_y_offset = 0;
-			xposition       = .5;
-			yposition       = .5;
-			
-			
-			
-			
 			
 			
 			JPanel image_panel = new JPanel(new BorderLayout());
 			
 			image_panel.add(image_canvas, BorderLayout.CENTER);
-			image_panel.add(xscrollbar, BorderLayout.SOUTH);
-			image_panel.add(yscrollbar, BorderLayout.EAST);
-
+		
 			
 			frame.getContentPane().add(image_panel);
 			frame.pack();
 			frame.setLocation(10, 10);
-			frame.setSize(canvas_xdim, canvas_ydim);
+			frame.setSize(extracted_xdim, extracted_ydim);
 			frame.setVisible(true);	
 			 
 		} 
@@ -401,89 +170,7 @@ public class Test
     {
         public synchronized void paint(Graphics g)
         {
-        	if(scale == 1.)
-			{
-				if(image_xdim <= canvas_xdim && image_ydim <= canvas_ydim)
-				{
-					image = original_image;
-					
-					if(image_xdim == canvas_xdim)
-						canvas_x_offset = 0;
-					else
-					{
-						int delta       = canvas_xdim - image_xdim;
-						canvas_x_offset = delta / 2;
-					}
-					
-					if(image_ydim == canvas_ydim)
-						canvas_y_offset = 0;
-					else
-					{
-						int delta       = canvas_ydim - image_ydim;
-						canvas_y_offset = delta / 2;
-					}
-				}
-				else
-				{
-					double range       = image_xdim - canvas_xdim;
-					image_x_offset  = (int)(range * xposition);
-					range           = image_ydim - canvas_ydim;
-					image_y_offset  = (int)(range * yposition);
-					
-					image           = original_image.getSubimage(image_x_offset, image_y_offset, canvas_xdim, canvas_ydim);
-					canvas_x_offset = 0;
-					canvas_y_offset = 0;
-				}
-			}
-			else
-			{
-				System.out.println("Scale is " + String.format("%.4f", scale));
-				System.out.println();
-				scaled_xdim = (int)(scale * image_xdim);
-				scaled_ydim = (int)(scale * image_ydim);
-				
-				AffineTransform scaling_transform = new AffineTransform();
-				scaling_transform.scale(scale, scale);
-				AffineTransformOp scale_op = new AffineTransformOp(scaling_transform, AffineTransformOp.TYPE_BILINEAR);
-				BufferedImage scaled_image = new BufferedImage(scaled_xdim, scaled_ydim, original_image.getType());
-				scaled_image               = scale_op.filter(original_image, scaled_image);
-				
-				if(scaled_xdim <= canvas_xdim && scaled_ydim <= canvas_ydim)
-				{
-				    image          = scaled_image;
-				    image_x_offset = 0;
-				    image_y_offset = 0;
-				    
-				    if(scaled_xdim == canvas_xdim)
-						canvas_x_offset = 0;
-					else
-					{
-						int delta       = canvas_xdim - scaled_xdim;
-						canvas_x_offset = delta / 2;
-					}
-					
-					if(scaled_ydim == canvas_ydim)
-						canvas_y_offset = 0;
-					else
-					{
-						int delta       = canvas_ydim - scaled_ydim;
-						canvas_y_offset = delta / 2;
-					}
-				}
-				else
-				{
-					double range       = scaled_xdim - canvas_xdim;
-					image_x_offset  = (int)(range * xposition);
-					range           = scaled_ydim - canvas_ydim;
-					image_y_offset  = (int)(range * yposition);
-					
-					image           = scaled_image.getSubimage(image_x_offset, image_y_offset, canvas_xdim, canvas_ydim);
-					canvas_x_offset = 0;
-					canvas_y_offset = 0;
-				}
-			}
-        	
-        	g.drawImage(image, canvas_x_offset, canvas_y_offset, this);
+        	g.drawImage(extracted_image, 0, 0, this);
         }
     }
 }
--- WaveletMapper.java
+++ WaveletMapper.java
@@ -23,6 +23,11 @@ public class WaveletMapper
 		int    ydim = src.length / xdim;
 		int [] dst  = new int[src.length];
 		
+	
+		for(int i = 0; i < src.length; i++)
+	    	dst[i] = src[i];
+	    
+		
 		int k = 0;
 		for (int i = 0; i < ydim /2; i++)
 		    for (int j = 0; j < xdim; j++)
@@ -36,6 +41,13 @@ public class WaveletMapper
 	    int ydim  = src.length / xdim;
 	    int dst[] = new int[src.length];
 	    
+	   
+	    /*
+	    for(int i = 0; i < src.length; i++)
+	    	dst[i] = src[i];
+	    */
+	    
+	    
 	    int k = 0;
 	    for(int i = 0; i < xdim / 2; i++)
 	        for (int j = 0; j < ydim; j++)
@@ -43,293 +55,6 @@ public class WaveletMapper
 	    return dst;
 	}
 	
-	/*
-	public static void daub4(int [] src, int offset, int xdim)
-	{
-		double [] c       = {0.4829629, 0.8365163, 0.2241438, -0.129409};
-		int    [] buffer = new int[xdim];
-		int i     = 0;
-		int k     = offset;
-		
-		if(xdim % 2 != 0)
-			xdim--;
-		
-		int m     = xdim / 2;
-		
-		for(int j = 0; j <= xdim - 4; j += 2)
-		{
-			buffer[i]     = (int)(c[0] * src[j + k] + c[1] * src[j + k + 1] + c[2] * src[j + k + 2] + c[3] * src[j + k + 3]);
-			buffer[i + m] = (int)(c[3] * src[j + k] - c[2] * src[j + k + 1] + c[1] * src[j + k + 2] - c[0] * src[j + k + 3]);	
-			i++;
-		}
-		buffer[i]     = (int)(c[0] * src[k + xdim - 2] + c[1] * src[k + xdim - 1] + c[2] * src[k] + c[3] * src[k + 1]);
-		buffer[i + m] = (int)(c[3] * src[k + xdim - 2] - c[2] * src[k + xdim - 1] + c[1] * src[k] - c[0] * src[k + 1]);
-		
-		for(i = 0; i < xdim; i++)
-		    src[i + k] = buffer[i]; 
-	}
-	
-	public static void inverse_daub4(int [] src, int offset, int xdim)
-	{
-		double [] c       = {0.4829629, 0.8365163, 0.2241438, -0.129409};
-		int    [] buffer = new int[xdim];
-		int i, j;
-		int k  = offset;
-		if(xdim % 2 != 0)
-			xdim--;
-		int m  = xdim / 2 - 1;
-		int n  = m + 1;
-		int[]  value = new int[4];
-		
-		buffer[0] = (int)(c[2] * src[k + m] + c[1] * src[k + xdim - 1] + c[0] * src[k + n] + c[3] * src[k + n]);
-		buffer[1] = (int)(c[3] * src[k + m] - c[0] * src[k + xdim - 1] + c[1] * src[k]     - c[2] * src[k + n]);
-		
-		for (i = 0, j = 2; i < m; i++)  
-	    {
-		    value[0] = src[i + k];
-		    value[1] = src[i + k + 1];
-		    value[2] = src[i + k + n];
-		    value[3] = src[i + k + n + 1];
-		
-		    buffer[j++] = (int)(c[2] * value[0] + c[1] * value[2] + c[0] * value[1] + c[3] * value[3]);
-		    buffer[j++] = (int)(c[3] * value[0] - c[0] * value[2] + c[1] * value[1] - c[2] * value[3]);
-	    }
-		
-		for(i = 0; i < xdim; i++)
-		    src[i + k] = buffer[i];
-	}
-	
-	public static int [] forward_transform(int [] src, int xdim)
-	{
-		int [] buffer = new int[src.length];
-		for(int i = 0; i < src.length; i++)
-			buffer[i] = src[i];
-		int ydim = src.length / xdim;
-		
-		// Assumes image dimension is even.
-		int min  = xdim;
-		// Could get stuck in a loop if dimension is a power of 2.
-		while(min % 2 == 0)
-			min /= 2;
-		//min *= 2;
-		
-		for(int i = 0; i < ydim; i++)
-		{
-			int offset = i * xdim;
-			for(int j = xdim; j >= min; j /= 2)
-			{
-				daub4(buffer, offset, j);
-			}
-		}
-		
-		int [] dst =  half_transpose(buffer, xdim);
-		
-		// Assumes image dimension is even.
-		min  = ydim;
-	    // Could get stuck in a loop if dimension is a power of 2.
-		while(min % 2 == 0)
-			min /= 2;
-		//min *= 2;
-		for(int i = 0; i < xdim /2; i++)  
-		{
-		    int offset = i * ydim;
-		    for(int j = ydim; j >= min; j /= 2)
-		    {
-		    	daub4(dst, offset, j);	
-		    }
-		}
-		
-		return dst;
-	}
-	
-	public static int [] inverse_transform(int [] src, int xdim)
-	{
-	    int ydim = src.length / xdim;
-	    int min = ydim;
-	    while (min % 2 == 0)
-	        min /= 2;
-	    int [] buffer = new int[src.length];
-	    for(int i = 0; i < src.length; i++)
-	    	buffer[i] = src[i];
-	    for(int i = 0; i < xdim / 2; i++)  
-	    {
-	        int offset = i * ydim;
-	        for(int j = min; j <= ydim; j *= 2)
-	        {
-	        	inverse_daub4(buffer, offset, j);
-	        }
-	    }
-
-	    int [] dst = half_transpose2(buffer, xdim);
-
-	    min = xdim;
-	    while(min % 2 == 0)
-	       min /= 2;
-	    min *= 2;
-
-	    for(int i = 0; i < ydim ; i++)
-	    {
-	        int offset = i * xdim;
-	    	for(int j = min; j <= xdim; j *= 2)
-	    	{
-	    		inverse_daub4(dst, offset, j);	
-	    	}
-	    }
-	    
-	    for(int i = 0; i < dst.length; i++)
-	    {
-	    	if(dst[i] < 0)
-	    		dst[i] = 0;
-	    	else if(dst[i] > 255)
-	    		dst[i] = 255;
-	    }
-	    
-	    return dst;
-	}
-	*/
-	
-	
-	/*
-	// Replaces a row or part of a row in the source.
-	public static void daub4(int [] src, int offset, int xdim, int min)
-	{
-		double [] c       = {0.4829629, 0.8365163, 0.2241438, -0.129409};
-		int    [] buffer = new int[xdim];
-		int i     = 0;
-		int k     = offset;
-		int m     = xdim / 2;
-		
-		for(int j = 0; j <= xdim - 4; j += 2)
-		{
-			buffer[i]     = (int)(c[0] * src[j + k] + c[1] * src[j + k + 1] + c[2] * src[j + k + 2] + c[3] * src[j + k + 3]);
-			buffer[i + m] = (int)(c[3] * src[j + k] - c[2] * src[j + k + 1] + c[1] * src[j + k + 2] - c[0] * src[j + k + 3]);	
-			i++;
-		}
-		buffer[i]     = (int)(c[0] * src[k + xdim - 2] + c[1] * src[k + xdim - 1] + c[2] * src[k] + c[3] * src[k + 1]);
-		buffer[i + m] = (int)(c[3] * src[k + xdim - 2] - c[2] * src[k + xdim - 1] + c[1] * src[k] - c[0] * src[k + 1]);
-		
-		for(i = 0; i < xdim; i++)
-		    src[i + k] = buffer[i]; 
-	}
-	
-	public static void inverse_daub4(int [] src, int offset, int xdim, int min)
-	{
-		double [] c       = {0.4829629, 0.8365163, 0.2241438, -0.129409};
-		int    [] buffer = new int[xdim];
-		int i, j;
-		int k  = offset;
-		int m  = xdim / 2 - 1;
-		int n  = m + 1;
-		int[]  value = new int[4];
-		
-		buffer[0] = (int)(c[2] * src[k + m] + c[1] * src[k + xdim - 1] + c[0] * src[k + n] + c[3] * src[k + n]);
-		buffer[1] = (int)(c[3] * src[k + m] - c[0] * src[k + xdim - 1] + c[1] * src[k]     - c[2] * src[k + n]);
-		
-		for (i = 0, j = 2; i < m; i++)  
-	    {
-		    value[0] = src[i + k];
-		    value[1] = src[i + k + 1];
-		    value[2] = src[i + k + n];
-		    value[3] = src[i + k + n + 1];
-		
-		    buffer[j++] = (int)(c[2] * value[0] + c[1] * value[2] + c[0] * value[1] + c[3] * value[3]);
-		    buffer[j++] = (int)(c[3] * value[0] - c[0] * value[2] + c[1] * value[1] - c[2] * value[3]);
-	    }
-		
-		for(i = 0; i < xdim; i++)
-		    src[i + k] = buffer[i];
-	}
-	
-	public static int [] forward_transform(int [] src, int xdim)
-	{
-		int [] buffer = new int[src.length];
-		for(int i = 0; i < src.length; i++)
-			buffer[i] = src[i];
-		int ydim = src.length / xdim;
-		
-		// Assumes image dimension is even.
-		int min  = xdim;
-		// Could get stuck in a loop if dimension is a power of 2.
-		while(min % 2 == 0)
-			min /= 2;
-		min *= 2;
-		
-		for(int i = 0; i < ydim; i++)
-		{
-			int offset = i * xdim;
-			for(int j = xdim; j >= min; j /= 2)
-			{
-				daub4(buffer, offset, j, min);
-			}
-		}
-		
-		int [] dst =  half_transpose(buffer, xdim);
-		
-		// Assumes image dimension is even.
-		min  = ydim;
-	    // Could get stuck in a loop if dimension is a power of 2.
-		while(min % 2 == 0)
-			min /= 2;
-		min *= 2;
-		for(int i = 0; i < xdim /2; i++)  
-		{
-		    int offset = i * ydim;
-		    for(int j = ydim; j >= min; j /= 2)
-		    {
-		    	daub4(dst, offset, j, min);	
-		    }
-		}
-		
-		return dst;
-	}
-	
-	public static int [] inverse_transform(int [] src, int xdim)
-	{
-	    int ydim = src.length / xdim;
-	    int min = ydim;
-	    while (min % 2 == 0)
-	        min /= 2;
-	    min  *= 2;
-	    int [] buffer = new int[src.length];
-	    for(int i = 0; i < src.length; i++)
-	    	buffer[i] = src[i];
-	    for(int i = 0; i < xdim / 2; i++)  
-	    {
-	        int offset = i * ydim;
-	        for(int j = min; j <= ydim; j *= 2)
-	        {
-	        	inverse_daub4(buffer, offset, j, min);
-	        }
-	    }
-
-	    int [] dst = half_transpose2(buffer, xdim);
-
-	    min = xdim;
-	    while(min % 2 == 0)
-	       min /= 2;
-	    min *= 2;
-
-	    for(int i = 0; i < ydim ; i++)
-	    {
-	        int offset = i * xdim;
-	    	for(int j = min; j <= xdim; j *= 2)
-	    	{
-	    		inverse_daub4(dst, offset, j, min);	
-	    	}
-	    }
-	    
-	    for(int i = 0; i < dst.length; i++)
-	    {
-	    	if(dst[i] < 0)
-	    		dst[i] = 0;
-	    	else if(dst[i] > 255)
-	    		dst[i] = 255;
-	    }
-	    
-	    return dst;
-	}
-	*/
-	
 	// Replaces a row or part of a row in the source.
 	public static void daub4(int [] src, int offset, int xdim)
 	{
@@ -387,13 +112,13 @@ public class WaveletMapper
 			buffer[i] = src[i];
 		int ydim = src.length / xdim;
 		
-		// Assumes image dimension is even.
 		int min  = xdim;
-		// Could get stuck in a loop if dimension is a power of 2.
 		while(min % 2 == 0)
 			min /= 2;
 		min *= 2;
 		
+		if(min < 4)
+			min = 4;
 		for(int i = 0; i < ydim; i++)
 		{
 			int offset = i * xdim;
@@ -409,6 +134,8 @@ public class WaveletMapper
 		while(min % 2 == 0)
 			min /= 2;
 		min *= 2;
+		if(min < 4)
+			min = 4;
 		for(int i = 0; i < xdim /2; i++)  
 		{
 		    int offset = i * ydim;
@@ -428,6 +155,8 @@ public class WaveletMapper
 	    while (min % 2 == 0)
 	        min /= 2;
 	    min  *= 2;
+	    if(min < 4)
+	    	min = 4;
 	    int [] buffer = new int[src.length];
 	    for(int i = 0; i < src.length; i++)
 	    	buffer[i] = src[i];
@@ -446,6 +175,8 @@ public class WaveletMapper
 	    while(min % 2 == 0)
 	       min /= 2;
 	    min *= 2;
+	    if(min < 4)
+	    	min = 4;
 
 	    for(int i = 0; i < ydim ; i++)
 	    {
